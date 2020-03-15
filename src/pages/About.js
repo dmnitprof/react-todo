81753a2b4ed00a93e184d113b7ef6556e4fc83ea
dmnitprof <54173914+dmnitprof@users.noreply.github.com>
2020-03-15 21:14:33 +0300

    make it better

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Приложение написано на React</h1>
+      <p className="lead">
+        Версия приложения <strong>1.0.1</strong>
+      </p>
+    </div>
+  </div>
+)
